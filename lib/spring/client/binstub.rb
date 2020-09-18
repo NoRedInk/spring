f46ec64dd82a74c597e694dc1c9e637c5a8512f1
Ary Borenszweig <asterite@gmail.com>
2020-09-18 12:03:35 -0300

    Fix binstub

--- lib/spring/client/binstub.rb
+++ lib/spring/client/binstub.rb
@@ -34,16 +34,16 @@ unless defined?(Spring)
   require 'bundler'
 
   lockfile = Bundler::LockfileParser.new(Bundler.default_lockfile.read)
-  spring = lockfile.specs.detect { |spec| spec.name == 'spring' }
+  spring = lockfile.specs.detect { |spec| spec.name == 'nrispring' }
   if spring
     Gem.use_paths Gem.dir, Bundler.bundle_path.to_s, *Gem.path
-    gem 'spring', spring.version
+    gem 'nrispring', spring.version
     require 'spring/binstub'
   end
 end
 CODE
 
-      OLD_BINSTUB = %{if !Process.respond_to?(:fork) || Gem::Specification.find_all_by_name("spring").empty?}
+      OLD_BINSTUB = %{if !Process.respond_to?(:fork) || Gem::Specification.find_all_by_name("nrispring").empty?}
 
       BINSTUB_VARIATIONS = Regexp.union [
         %{begin\n  load File.expand_path('../spring', __FILE__)\nrescue LoadError\nend\n},
